7a0cb622d614df1ace0225422aceef0a0cbf9701
t2money11 <453462638@qq.com>
2019-08-26 09:42:37 +0000

    added a argument to the custom control

--- webapp/customControl/MyHeading.js
+++ webapp/customControl/MyHeading.js
@@ -1,11 +1,16 @@
 sap.ui.define(["sap/ui/core/Control"], function (Control) {
 	return Control.extend("gitTest.GitTest.customControl.MyHeading", {
-		properties: {},
-		aggregations: {},
-		events: {},
-		methods: {},
+		metadata: {
+			properties: {
+				"text": ""
+			},
+			aggregations: {},
+			events: {},
+			methods: {}
+		},
+		init: function(){},
 		renderer: function(oRm, oControl){
-			oRm.write("<h1>This is Karl's Custom Control</h1>");
+			oRm.write("<h1>" + oControl.getText() + "</h1>");
 		}
 	});
 });
