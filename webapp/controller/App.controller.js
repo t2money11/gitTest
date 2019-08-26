67455677573a65aa53f3c76ed32ff38b0cfa7c2b
t2money11 <453462638@qq.com>
2019-08-26 07:28:33 +0000

    added 2 pages

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -5,7 +5,21 @@ sap.ui.define([
 
 	return Controller.extend("gitTest.GitTest.controller.App", {
 		onInit: function () {
-
+			
+			var searchData = new sap.ui.view({
+				id: "searchData",
+				type: sap.ui.core.mvc.ViewType.XML,
+				viewName: "gitTest.GitTest.view.SearchData"
+			});
+			
+			var detail = new sap.ui.view({
+				id: "detail",
+				type: sap.ui.core.mvc.ViewType.XML,
+				viewName: "gitTest.GitTest.view.Detail"
+			});
+			
+			this.getView().byId("app").addPage(searchData);
+			this.getView().byId("app").addPage(detail);
 		}
 	});
 });
